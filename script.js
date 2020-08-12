66ad02d630974970237c1d61d6a1eab0e3e8e49b
Olubiyi Emmanuel <42422184+coded1guy@users.noreply.github.com>
2020-08-12 11:24:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
 // Set the date we're counting down to
 var countDownDate = new Date("Dec 1, 2020 12:00:00").getTime();
 
